d187c412e87d8525116d1682d2902c32f5ceb764
chris turina <chris.turina@gmail.com>
2017-11-03 11:36:20 -0700

    menu part 2 complete

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@
 $(document).ready(function() {
   $(".menu").click(function() {
     $(".menu").toggleClass("x-active");
+    $(".about-me").toggle();
+    $(".my-work").toggle();
+    $(".contact-page").toggle();
 
   })
 });
